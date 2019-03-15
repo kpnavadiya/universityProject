34c1e69eff8990652137c5b83f1fe1bc6d80725e
kpnavadiya <krunalnavadiya786@gmail.com>
2019-03-15 22:20:34 +0530

    Add File

--- install.sh
+++ install.sh
@@ -1,14 +1,32 @@
 # to be executed on start
-docker exec -it cli peer chaincode install -n mycc -p github.com/chaincode -v v0
+docker exec -it cli peer chaincode install -n mycc -p github.com/chaincode/go -v v0
 docker exec -it cli peer chaincode instantiate -o orderer0.uni.com:7050 -C mychannel -n mycc github.com/chaincode -v v0 -c '{"Args": []}'
 
-# CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/admin.example.com/users/Admin@admin.example.com/msp CORE_PEER_ADDRESS=peer0.admin.example.com:7051 CORE_PEER_LOCALMSPID="AdminMSP" CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/admin.example.com/peers/peer0.admin.example.com/tls/ca.crt peer chaincode install -n mycc -p github.com/chaincode -v v0
+# install chaincode on peer0.universitymember.uni.com
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/universitymember.uni.com/users/Admin@universitymember.uni.com/msp 
+export CORE_PEER_ADDRESS=peer0.universitymember.uni.com:7051 
+export CORE_PEER_LOCALMSPID="UniversityMemberMSP" 
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/universitymember.uni.com/peers/peer0.universitymember.uni.com/tls/ca.crt 
+peer chaincode install -n mycc -p github.com/chaincode/go -v v0
 
-# # Join peer0.manager.example.com to the channel
-# CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/manager.example.com/users/Admin@manager.example.com/msp CORE_PEER_ADDRESS=peer0.manager.example.com:7051 CORE_PEER_LOCALMSPID="ManagerMSP" CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/manager.example.com/peers/peer0.manager.example.com/tls/ca.crt peer chaincode install -n mycc -p github.com/chaincode -v v0
+# # # install chaincode onpeer1.universitymember.uni.com .
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/universitymember.uni.com/users/Admin@universitymember.uni.com/msp 
+export CORE_PEER_ADDRESS=peer1.universitymember.uni.com:7051 
+export CORE_PEER_LOCALMSPID="UniversityMemberMSP" 
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/universitymember.uni.com/peers/peer1.universitymember.uni.com/tls/ca.crt 
+peer chaincode install -n mycc -p github.com/chaincode/go -v v0
 
 
-# # Join peer0.vendor.example.com to the channel
-# CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/vendor.example.com/users/Admin@vendor.example.com/msp CORE_PEER_ADDRESS=peer0.vendor.example.com:7051 CORE_PEER_LOCALMSPID="VendorMSP" CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/vendor.example.com/peers/peer0.vendor.example.com/tls/ca.crt peer chaincode install -n mycc -p github.com/chaincode -v v0
-# # Join peer0.user.example.com to the channel
-# CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/user.example.com/users/Admin@user.example.com/msp CORE_PEER_ADDRESS=peer0.user.example.com:7051 CORE_PEER_LOCALMSPID="UserMSP" CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/user.example.com/peers/peer0.user.example.com/tls/ca.crt peer chaincode install -n mycc -p github.com/chaincode -v v0
+# # # install chaincode onpeer0.publicmember.uni.com .
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/publicmember.uni.com/users/Admin@publicmember.uni.com/msp 
+export CORE_PEER_ADDRESS=peer0.publicmember.uni.com:7051 
+export CORE_PEER_LOCALMSPID="PublicMemberMSP" 
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/publicmember.uni.com/peers/peer0.publicmember.uni.com/tls/ca.crt 
+peer chaincode install -n mycc -p github.com/chaincode/go -v v0
+
+# # # install chaincode on peer1.publicmember.uni.com.
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/publicmember.uni.com/users/Admin@publicmember.uni.com/msp 
+export CORE_PEER_ADDRESS=peer1.publicmember.uni.com:7051 
+export CORE_PEER_LOCALMSPID="PublicMemberMSP" 
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/publicmember.uni.com/peers/peer1.publicmember.uni.com/tls/ca.crt 
+peer chaincode install -n mycc -p github.com/chaincode/go -v v0
--- joinchannel.sh
+++ joinchannel.sh
@@ -6,7 +6,7 @@ export CORE_PEER_ADDRESS=peer0.universitymember.uni.com:7051
 export CORE_PEER_LOCALMSPID="UniversityMemberMSP" 
 export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/universitymember.uni.com/peers/peer0.universitymember.uni.com/tls/ca.crt 
 export O_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/uni.com/orderers/orderer0.uni.com/msp/tlscacerts/tlsca.uni.com-cert.pem
-peer channel create -o orderer0.uni.com:7050 -c $CHANNEL_NAME -f ./config/channel.tx --tls --cafile $O_CA
+peer channel create -o orderer0.uni.com:7050 -c $CHANNEL_NAME -f ./configs/channel.tx --tls --cafile $O_CA
 
 # # # Join peer0.universitymember.uni.com to the channel
 export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/universitymember.uni.com/users/Admin@universitymember.uni.com/msp 
